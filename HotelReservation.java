c2c4fe6e54c9df9fe9278a8fc5702e29915b6f4d
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-15 20:28:21 +0530

    Added: Ability to add Hotel with Name and rates for Regular Customer

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,7 +1,45 @@
 package com.bl.assignment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservation {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation Program");
+	
+	List<Hotel> hotelList;
+	
+	public HotelReservation() {
+		hotelList = new ArrayList<>();
+	}
+	
+	/**
+	 * UC 1
+	 * 
+	 * @param name
+	 * @param regularWeekday
+	 */
+	public void addHotel(String name, int regularWeekday, int regularWeekEnd) {
+		Hotel hotel = new Hotel(name, regularWeekday, regularWeekEnd);
+		hotelList.add(hotel);
+	}
+	
+	/**
+	 * PRINT
+	 * 
+	 */
+	public void printHotels() {
+		for(Hotel hotel : hotelList) {
+			System.out.println("Rate for Hotel " + hotel.getName() + " for regular customer for weekday is : " 
+							+ hotel.getRegularWeekday() + " and for weekend is : " + hotel.getRegularWeekEnd());
+			System.out.println("");
+		}
+	}
+	
+	/**
+	 * SIZE
+	 * 
+	 * @return
+	 */
+	public Integer size() {
+		return hotelList.size();
 	}
 }
